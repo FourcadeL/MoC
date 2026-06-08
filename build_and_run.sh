61409cedf7f458d5008cefabee8990d613c59758
FourcadeL <louis.fourcade142@gmail.com>
2026-06-08 09:58:59 +0200

    Build and run script

---
+++ build_and_run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Default build and run script
+
+# Go into the script's directory
+cd "$(dirname "$0")"
+
+# Create build dir if it does not exists
+if [ ! -d "build" ]; then
+	mkdir build
+fi
+
+cd build && cmake .. && make
+
+if [ $? = 0 ]; then
+	./moc
+fi
